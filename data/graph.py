29079259de577c7138a1c6eb7ea62dbf3bc43e37
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-10-24 22:33:11 -0400

    graphing code

---
+++ data/graph.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import csv
+  
+time = []
+mag_x = []
+mag_y = []
+mag_z = []
+vertical_lines = []
+  
+with open('3ms.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+    for i in range(len(data)-1):
+        if(i != 0):
+            if data[i-1][1] != data[i][1]:
+                vertical_lines = vertical_lines + [float(data[i][0])]
+      
+    for row in data:
+        time.append(float(row[0]))
+        mag_x.append(float(row[2]))
+        mag_y.append(float(row[3]))
+        mag_z.append(float(row[4]))
+  
+
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+ax.plot(time, mag_x, label = "X")
+ax.plot(time, mag_y, label = "Y")
+ax.plot(time, mag_z, label = "Z")
+
+
+
+
+ax.vlines(vertical_lines, -.25, 1.5, linestyles='dashed', colors='red')
+
+
+
+
+
+plt.xlabel('Time (ms)')
+plt.title('Magnetorquer Magnetometer Readings 3ms On 3ms Off')
+plt.legend()
+plt.show()
